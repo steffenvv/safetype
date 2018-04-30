1bbb8590bd87f1819680717b735a45916eb8ba83
Steffen Viken Valvåg <stvikenv@microsoft.com>
2018-04-30 10:00:44 +0200

    Update naming in person example.

--- src/examples/person.ts
+++ src/examples/person.ts
@@ -2,29 +2,29 @@ import { aString, aStringUnion, anObject } from "..";
 
 // Person example
 
-const phoneValidator = anObject({
+const aPhone = anObject({
     phoneNumber: aString,
     phoneType: aStringUnion("Home", "Business", "Mobile", "Unknown")
 });
 
-const personValidator = anObject({
+const aPerson = anObject({
     name: aString,
-    phones: phoneValidator.array.orUndefined
+    phones: aPhone.array.orUndefined
 });
 
 /* Derive the Person type from its validation code, to keep them in sync. */
-type Person = ReturnType<typeof personValidator.validate>;
+type Person = ReturnType<typeof aPerson.validate>;
 
 const p: Person = {
     name: "Bob",
     phones: [{ phoneNumber: "123", phoneType: "Mobile" }]
 };
 
-personValidator.validate(p); /* Throws if p is not a Person */
+aPerson.validate(p); /* Throws if p is not a Person */
 
 const q = JSON.parse(JSON.stringify(p));
 
-if (personValidator.isValid(q)) {
+if (aPerson.isValid(q)) {
     /* Validates that q is a Person and narrows its type from any to Person */
     console.log(`Hello, ${q.name}`);
 }
