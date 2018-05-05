af04503379a6e8cfe6c924a636294b27f2f66bab
Steffen Viken Valvåg <stvikenv@microsoft.com>
2018-05-05 09:06:23 +0200

    Add test for typings of unions.

--- src/__tests__/types.tests.ts
+++ src/__tests__/types.tests.ts
@@ -128,4 +128,36 @@ describe("type inference", () => {
         ].join("\n");
         expect(compileModule(sourceCode)).toMatchSnapshot();
     });
+
+    it("works correctly for unions", () => {
+        const sourceCode = [
+            ...commonSourceCode,
+            `import { anObject, aString, aNumber, aStringLiteral } from ".";
+             export const anOutcome = anObject({
+                 kind: aStringLiteral("Success"),
+                 data: aString.array,
+                 timestamp: aNumber
+             }).or(
+                 anObject({
+                     kind: aStringLiteral("Error"),
+                     error: aString,
+                     stackTrace: aString.orUndefined
+                 })
+             );            
+             export type Outcome = InferType<typeof anOutcome>;`
+        ].join("\n");
+        expect(compileModule(sourceCode)).toMatchSnapshot();
+
+        /* Inferred type:
+        type Outcome = {
+            readonly kind: "Success";
+            readonly data: ReadonlyArray<string>;
+            readonly timestamp: number;
+        } | {
+            readonly kind: "Error";
+            readonly error: string;
+            readonly stackTrace?: string | undefined;
+        }
+        */
+    });
 });
